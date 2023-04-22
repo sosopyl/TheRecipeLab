a7760bb7734c6d18f7871dfe601a34dc81692c42
SosoPyl <elizavetaplieva15@gmail.com>
2023-04-22 15:32:16 +0300

    first

---
+++ script.js
@@ -0,0 +1,32 @@
+let searchicon = document.querySelector('#searchicon')
+let searchinput = document.querySelector('#searchinput')
+searchicon.addEventListener('click', function(){
+    searchicon.classList.add('hidding')
+    searchinput.classList.remove('hidding')
+})
+searchinput.addEventListener('blur', function(){
+    searchicon.classList.remove('hidding')
+    searchinput.classList.add('hidding')
+    searchinput.value = " "
+})
+searchinput.addEventListener('keyup',event =>{
+    if(event.code === 'Enter'){
+        searchicon.classList.remove('hidding')
+        searchinput.classList.add('hidding')
+        searchinput.value = " "
+    }
+})
+
+let reasonbtn = document.querySelectorAll('.reasonbtn')
+
+for(let i = 0; i < reasonbtn.length; i++){
+    reasonbtn[i].addEventListener('click', function(){
+        this.classList.toggle("active")
+        let panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+          } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+          }
+    })
+}
